bb1c705459d9271b1822f2a53fa81e5b5194772a
Blazej Golaszewski <b.golaszewski@kainos.com>
2015-04-22 22:37:29 +0200

    added parametrizationa and removing empty rows before sorting

--- rowsSorting.js
+++ rowsSorting.js
@@ -1,24 +1,47 @@
-function sortRows() {
-  var WIDTH_COLUMN_COUNT = 9;
+var SORT_COLUMN = 0;
+
+function sortBusy() {
+  sortRows("BUSY");  
+}
+
+function sortTiry() {
+  sortRows("TABELA"); 
+}
+
+function sortRows(sheetName) {
+  var WIDTH_COLUMN_COUNT = getColumnNumber("PAL", 2) - 1;
   var START_ROWN_NUM = 2;
   
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  SpreadsheetApp.setActiveSheet(ss.getSheets()[1]);
-
-  var sheet = SpreadsheetApp.getActiveSheet();
+ 
+  var sheet = ss.getSheetByName(sheetName);
+  
   SpreadsheetApp.setActiveSheet(sheet)
   Logger.log(sheet.getName());
   var numRowsWithoutHeader = sheet.getDataRange().getNumRows() - 1;
   var firstRange = sheet.getRange(START_ROWN_NUM, 1, numRowsWithoutHeader, WIDTH_COLUMN_COUNT);
-  var secondRange = sheet.getRange(START_ROWN_NUM, WIDTH_COLUMN_COUNT, numRowsWithoutHeader, WIDTH_COLUMN_COUNT);
+  var secondRange = sheet.getRange(START_ROWN_NUM, WIDTH_COLUMN_COUNT + 1, numRowsWithoutHeader, WIDTH_COLUMN_COUNT);
   var firstHalf = firstRange.getValues();
   var secondHalf = secondRange.getValues();
   
   var dataToSort = firstHalf.concat(secondHalf);
+  var dataToSortWithoutDuplicates = [];
+  
+  SORT_COLUMN = getColumnOffset("PLZ", 1);
+  
+  Logger.log("Remove empty lines");
+  
+  for (row in dataToSort) {
+    if (dataToSort[row][SORT_COLUMN].length > 0) {
+      dataToSortWithoutDuplicates.push(dataToSort[row]);
+    }
+  }
+  
+  dataToSort = dataToSortWithoutDuplicates;
   
   Logger.log("Start sort");
+  
   dataToSort.sort(function(a, b){
-    var SORT_COLUMN = 4;
     if (a[SORT_COLUMN] > b[SORT_COLUMN]){
       return 1;
     }
@@ -34,10 +57,43 @@ function sortRows() {
   });
   Logger.log("Added counter");
   
-  firstHalf = dataToSort.slice(0, firstHalf.length);
-  secondHalf = dataToSort.slice(firstHalf.length);
+  var halfDataArray = Math.floor(dataToSort.length/2);
+
+  firstHalf = dataToSort.slice(0, halfDataArray);
+  secondHalf = dataToSort.slice(dataToSort.length - halfDataArray);
   Logger.log("Revert copy");
   
-  firstRange.setValues(firstHalf);
-  secondRange.setValues(secondHalf);
+  firstRange.clearContent();
+  var newFirstRange = firstRange.offset(0, 0, firstHalf.length, WIDTH_COLUMN_COUNT)
+  newFirstRange.setValues(firstHalf);
+  
+  secondRange.clearContent();
+  var newSecondRange = secondRange.offset(0, 0, secondHalf.length, WIDTH_COLUMN_COUNT)
+  newSecondRange.setValues(secondHalf);
 };
+
+function getColumnOffset(columnName, numberOfOccurance) {
+  var occuranceCount = 0;
+  var occurance = numberOfOccurance ? numberOfOccurance : 1;
+
+  lastColumn = SpreadsheetApp.getActiveSheet().getLastColumn();
+  var range = SpreadsheetApp.getActiveSheet().getRange(1,1,1,lastColumn);
+
+  for (var i = 0; i < range.getLastColumn(); i++) {
+    if (range.offset(0, i, 1, 1).getValue() == columnName) {
+      occuranceCount++;
+      
+      if (occuranceCount === numberOfOccurance) {
+        return i;
+      }
+    } 
+  }
+}
+
+function setCarBackground() {
+  
+}
+
+function getColumnNumber(columnName, numberOfOccurance) {
+  return getColumnOffset(columnName, numberOfOccurance) + 1;
+}
